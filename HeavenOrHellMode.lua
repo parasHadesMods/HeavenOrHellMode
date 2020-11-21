5f0ad3bd7958bd68c3e20931a94c82f36c8f3819
paradigmsort <paradigmsort+hades@gmail.com>
2020-11-21 11:57:49 -0800

    Initial commit.

---
+++ HeavenOrHellMode.lua
@@ -0,0 +1,17 @@
+ModUtil.RegisterMod("HeavenOrHellMode")
+
+ModUtil.WrapBaseFunction("CreateNewHero", function(baseFunc, prevRun, args)
+  local hero = baseFunc(prevRun, args)
+  print("hello")
+  if GetNumMetaUpgrades("NoInvulnerabilityShrineUpgrade") >= 1 then
+    AddIncomingDamageModifier( hero, {
+      Name = "HeavenOrHellMode",
+      GlobalMultiplier = 10000
+    })
+    AddOutgoingDamageModifier( hero, {
+      Name = "HeavenOrHellMode",
+      GlobalMultiplier = 10000
+    })
+  end
+  return hero
+end, HeavenOrHellMode)
